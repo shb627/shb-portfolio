a17faa7d76d77e1b8832f003909dd382c0729aae
shb627 <shb0627@naver.com>
2026-05-11 09:28:12 +0900

    카테고리 스티커

--- js/pages/modal.js
+++ js/pages/modal.js
@@ -1,26 +1,20 @@
-modalContent.innerHTML = `
-  <h2>로그인</h2>
-  <input type="text">
-`;
+document.addEventListener("DOMContentLoaded", () => {
+  const modalLinks = document.querySelectorAll(".modal-link");
+  const modal = document.querySelector("#modal");
+  const modalImage = document.querySelector("#modalImage");
 
-const openModal = document.getElementById("openModal");
-const closeModal = document.getElementById("closeModal");
-const modalContent = document.getElementById("modalContent");
+  modalLinks.forEach((link) => {
+    link.addEventListener("click", (e) => {
+      e.preventDefault(); // 새탭/링크 이동 막기
 
-openModal.addEventListener("click", () => {
-  modal.classList.add("show");
-  document.body.style.overflow = "hidden"; // 배경 스크롤 막기
-});
+      modalImage.src = link.getAttribute("href");
+      modal.classList.add("active");
+    });
+  });
 
-closeModal.addEventListener("click", () => {
-  modal.classList.remove("show");
-  document.body.style.overflow = "auto";
-});
-
-// 배경 눌렀을 때 닫기
-modalContent.addEventListener("click", (e) => {
-  if (e.target === modal) {
-    modal.classList.remove("show");
-    document.body.style.overflow = "auto";
-  }
+  modal.addEventListener("click", (e) => {
+    if (e.target === modal) {
+      modal.classList.remove("active");
+    }
+  });
 });
